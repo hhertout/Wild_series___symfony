8b7a818e7d4d953aa36f0f379612a2168ed5e602
hhertout <hugues.hertout@gmail.com>
2022-12-03 15:46:27 +0100

    glitch effect

--- assets/app.js
+++ assets/app.js
@@ -22,5 +22,82 @@ require('bootstrap');
 // require('bootstrap/js/dist/tooltip');
 // require('bootstrap/js/dist/popover');
 
+import { gsap } from "gsap";
+
+const tl = gsap.timeline({
+    repeat: -1, 
+    repeatDelay: Math.random() * 7
+});
+
+tl.to('.glitch', {
+    duration:0.01, 
+    skewX:70,
+    ease: "power4.easeInOut"
+})
+.to('.glitch', {
+    duration: 0.04,
+    skewX:0,
+    ease: "power4.easeInOut"
+})
+.to('.glitch', {
+    duration: 0.01,
+    opacity:0
+})
+.to('.glitch', {
+    duration: 0.01,
+    opacity:1
+})
+.to('.glitch', {
+    duration: 0.01,
+    x:-10,
+    ease: "rough({ template: none.out, strength: 1, points: 20, taper: none, randomize: true, clamp: false})"
+})
+.to('.glitch', {
+    duration: 0.01,
+    x:0
+})
+.to('.top', {
+    duration: 0.01,
+    opacity: 0,
+    x:-30,
+    ease: "rough({ template: none.out, strength: 1, points: 20, taper: none, randomize: true, clamp: false})"
+})
+.to('.bottom', {
+    duration: 0.01,
+    x:20,
+    ease: "rough({ template: none.out, strength: 1, points: 20, taper: none, randomize: true, clamp: false})"
+})
+.to('#txt', {
+    duration: 0.01, 
+    scale: Math.random() * 2
+})
+.to('#txt', {
+    duration: 0.01,
+    scale: 1,
+    ease: "rough({ template: none.out, strength: 1, points: 20, taper: none, randomize: true, clamp: false})"
+}, "+=0.02")
+
+.to('.top', {
+    duration: 0.02,
+    x:0,
+    ease: "rough({ template: none.out, strength: 1, points: 20, taper: none, randomize: true, clamp: false})"
+})
+.to('.bottom', {
+    duration: 0.02,
+    x:0,
+    ease: "rough({ template: none.out, strength: 1, points: 20, taper: none, randomize: true, clamp: false})"
+})
+
+.to('.glitch', {
+    duration: 0.02,
+    scaleY: Math.random() * 2,
+    ease: "Power4.easeInOut"
+})
+.to('.glitch', {
+    duration: 0.04,
+    scaleY:1,
+    ease: "Power4.easeInOut"
+})
+
 
 
